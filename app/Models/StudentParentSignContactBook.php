831428f17822db92f07b632956367d727c0d7b67
davis1233798 <davis1233798@gmail.com>
2023-12-31 02:21:20 +0800

    Add AsSource trait and fillable attributes to StudentParentSignContactBook model

--- app/Models/StudentParentSignContactBook.php
+++ app/Models/StudentParentSignContactBook.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
 
 class StudentParentSignContactBook extends Model
 {
     use HasFactory;
+    use AsSource;
+    protected $fillable = [
+        'student_id',
+        'communication',
+        'parent_infos_id',
+        'reply',
+        'sign_time',
+        'content',
+        'remark'
+    ];
+
 }
