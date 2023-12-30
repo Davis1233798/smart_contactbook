46f21e12103df43da495265b45a8a80530099819
davis1233798 <davis1233798@gmail.com>
2023-12-31 02:08:32 +0800

    Add logging for request data in ParentResponseController

--- app/Http/Controllers/ParentResponseController.php
+++ app/Http/Controllers/ParentResponseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ParentInfo;
 use App\Models\StudentParentSignContactBook;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ParentResponseController extends Controller
 {
@@ -20,6 +21,7 @@ class ParentResponseController extends Controller
 
     public function submitResponse(Request $request)
     {
+        Log::info($request->all());
         // 驗證輸入數據
         $validated = $request->validate([
             'parent_id' => 'required|exists:parent_infos,id',
